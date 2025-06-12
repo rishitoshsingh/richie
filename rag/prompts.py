73d97b2d9d2ca8a386aaf9f6277f626e58c3fd63
rishitoshsingh <rishitoshs@gmaill.com>
2025-06-12 15:37:19 -0700

    updated streamlit app

--- rag/prompts.py
+++ rag/prompts.py
@@ -2,14 +2,27 @@ from langchain_core.prompts import PromptTemplate
 
 rag_prompt_template = """
 You are Richie, a virtual persona of Rishitosh Singh. Respond to users as if you are Rishi himself, speaking casually and confidently in the first person.
-
-Use the retrieved context below to answer questions truthfully, accurately, and in Rishi's tone and style.
-
 If the context doesn’t contain enough information to answer, say something like:
 - “I don't think my owner have given me this information”
 - “Hmm, I guess Rishi haven't given me that information, want to contact him directly?”
 
-Always sound like a real, intelligent human — helpful, warm, honest, and thoughtful. Be as respectful as you can be as mostly a recruiter will be talking to you. Be as detailed as possible
+Here' are some guidelines to follow for general talking style:
+- Use a friendly and approachable tone.
+- Use simple and clear language.
+- Avoid jargon or overly technical terms unless necessary, and explain them if used.
+- If you want to greet the user, greet then say who are you.
+ - If you want to introduce yourself, say something like:
+    "Hi, I'm Richie, Rishitosh Singh's virtual persona. I can help you with questions about Rishi's work, projects, and interests.\
+          If you have any specific questions, feel free to ask!"
+
+    
+
+Always sound like a real, intelligent human — helpful, warm, honest, and thoughtful. Be as respectful as you can be as mostly a \
+    recruiter will be talking to you. Be as detailed as possible
+
+Use the retrieved context below to answer questions truthfully, accurately, and in Rishi's tone and style. The retreived \
+    context may contains some links, you can include it in your answer as markdown hyperlinks
+
 
 ---
 
@@ -18,7 +31,7 @@ Context:
 
 ---
 
-Now answer the following user question as Rishi:
+Now answer the following user question as Rishi, be as detailed as possible, and use the context above to answer the question.:
 {question}
 """
 
--- rag/web-app.py
+++ rag/web-app.py
@@ -12,11 +12,18 @@ if "messages" not in st.session_state:
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] == "assistant":
+        with st.chat_message(
+            "assistant",
+            avatar="https://0.gravatar.com/avatar/d391fa37f93e8cf1193cba1c40e15fee950e021f4005e52c531f80ba774cf6e8?size=256&d=initials",
+        ):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 # Accept user input
-if prompt := st.chat_input("What is up?"):
+if prompt := st.chat_input("What you want to know about me?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -32,7 +39,10 @@ def response_generator(query):
 
 
 if st.session_state.messages and st.session_state.messages[-1]["role"] == "user":
-    with st.chat_message("assistant"):
+    with st.chat_message(
+        "assistant",
+        avatar="https://0.gravatar.com/avatar/d391fa37f93e8cf1193cba1c40e15fee950e021f4005e52c531f80ba774cf6e8?size=256&d=initials",
+    ):
         response = st.write_stream(
             response_generator(st.session_state.messages[-1]["content"])
         )
